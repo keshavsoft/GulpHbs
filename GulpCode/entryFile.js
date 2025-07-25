71297c42838416631503413badffcba09d6c6dd7
krishnasanjay27 <Krishnasanjaybangaru@gmail.com>
2025-07-25 19:35:48 +0530

    connected apiversion and tablename for calendar

--- GulpCode/entryFile.js
+++ GulpCode/entryFile.js
@@ -7,6 +7,7 @@ dotenv.config();
 const StartFunc = ({ inDistPath }) => {
     LocalFuncForOnArray({ inDistPath });
     LocalFuncForRead({ inDistPath });
+    LocalFuncForCalendar({ inDistPath });
 };
 
 const LocalFuncForOnArray = ({ inDistPath }) => {
@@ -30,6 +31,21 @@ const LocalFuncForRead = ({ inDistPath }) => {
     const LocalDistPath = inDistPath;
 
     const filePath = `${LocalDistPath}/Js/Read/Config.json`;
+
+    const content = fse.readFileSync(filePath, 'utf-8');
+    const contentAsJson = JSON.parse(content);
+    contentAsJson.columns = CommonSchemaJson.columns;
+    contentAsJson.TableName = contentAsJson.TableName.replace("$TableName", CommonSchemaJson.tableName);
+    contentAsJson.TableName = contentAsJson.TableName.replace("$ApiVersion", process.env.VERSION);
+     contentAsJson.DataTableOptions = CommonSchemaJson.DataTableOptions;
+    
+    fse.writeFileSync(filePath, JSON.stringify(contentAsJson), 'utf-8');
+};
+
+const LocalFuncForCalendar = ({ inDistPath }) => {
+    const LocalDistPath = inDistPath;
+
+    const filePath = `${LocalDistPath}/Js/Calendar/Config.json`;
 
     const content = fse.readFileSync(filePath, 'utf-8');
     const contentAsJson = JSON.parse(content);

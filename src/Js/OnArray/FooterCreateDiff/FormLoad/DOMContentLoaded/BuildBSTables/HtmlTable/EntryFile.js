ada4d4a52361cd0c7ec20c545691f89eed4ae99c
sharath0036 <ksharathbabu2@gmail.com>
2025-07-31 17:32:30 +0530

    feat: Implement footer creation functionality with dynamic table updates

--- src/Js/OnArray/FooterCreateDiff/FormLoad/DOMContentLoaded/BuildBSTables/HtmlTable/EntryFile.js
+++ src/Js/OnArray/FooterCreateDiff/FormLoad/DOMContentLoaded/BuildBSTables/HtmlTable/EntryFile.js
@@ -1,15 +1,15 @@
+// import commonConfigJson from '../../../../../Config.json' with {type: 'json'};
 import { StartFunc as StartFuncFromOnPostBody } from "./OnPostBody/EntryFile.js";
-// import ConfigJson from '../../../../../Config.json' with {type: 'json'};
 
 const StartFunc = () => {
     var $table = $('#table');
 
     let LocalConfig = {};
+
     LocalConfig.onPostBody = StartFuncFromOnPostBody;
-    // LocalConfig.autoFocus = "Latitude";
-    // LocalConfig.autoFocus = ConfigJson?.DataTableOptions?.Header?.autoFocus
 
     $table.bootstrapTable(LocalConfig);
+
 };
 
 export { StartFunc };
--- src/Js/OnArray/verticalCreate/FormLoad/DOMContentLoaded/AddListeners/SaveButtonId/FetchFromFuncs/AfterFetch/entryFile.js
+++ src/Js/OnArray/verticalCreate/FormLoad/DOMContentLoaded/AddListeners/SaveButtonId/FetchFromFuncs/AfterFetch/entryFile.js
@@ -5,7 +5,7 @@ let StartFunc = async ({ inResponse }) => {
     let jVarLocalResponse = await inResponse;
 
     if (jVarLocalResponse.status === 200) {
-        let jVarLocalSavedPk = await jVarLocalResponse.json();
+        let jVarLocalSavedPk = await jVarLocalResponse.text();
         Status200({ inRowPk: jVarLocalSavedPk });
     };
 
